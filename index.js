f5602b9447bdd8d8e856b00f3282e106aaad5ecc
skytool <albert.do@aol.com>
2023-05-13 21:10:11 -0500

    I couldnot find what is wrong

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+    .then(renderBooks => console.log(json));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = `<h2>${book.name}</h2>`;
     main.appendChild(h2);
   });
 }
